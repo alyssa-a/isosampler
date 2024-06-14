efb5b54f3f1500a5517b94a598956e7976f4e678
Alyssa Aiello <alyssaaiello4@gmail.com>
2024-06-13 23:17:13 -0500

    Table of contents styling

--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,9 +1,31 @@
 'use client';
 
-export default function TOC() {
-    return (
-        <div id="toc">
-            <h2>On This Page</h2>
-        </div>
-    )
+import { useState, useEffect } from "react";
+
+export default function TableOfContents() {
+    const [headings, setHeadings] = useState();
+
+    useEffect(() => {
+        const elements = Array.from(document.querySelectorAll("#primary h2"))
+            .map((elem) => ({
+                text: elem.innerText,
+            }));
+        
+        setHeadings(elements);
+    }, []);
+
+    if (headings && headings.length > 0 ) {
+        return (
+            <div id="toc">
+                <h2>On This Page</h2>
+                <ul>
+                    {headings.map(heading => (
+                        <li key={heading.text}>
+                            <a href="#">{heading.text}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
