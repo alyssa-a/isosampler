349e68c830769891c2333e10352e3f7de8a51250
Alyssa Aiello <alyssaaiello4@gmail.com>
2024-06-17 01:28:40 -0500

    Swap logo for png

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '@/public/images/isotech-logo.svg';
+import logo from '@/public/images/isotech-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
@@ -50,6 +50,8 @@ export default function Header() {
                             priority
                             src={logo}
                             alt="Isotech a Stratum Reservoir brand"
+                            width="225"
+                            height="56"
                         />
                     </Link>
                 </div>
