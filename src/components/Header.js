4b31d02af68f80f08e0ac1b230b8f4eeef6132ea
Alyssa Aiello <alyssaaiello4@gmail.com>
2024-06-16 21:55:10 -0500

    Fixed hero image placement on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,14 +59,14 @@ export default function Header() {
                         </button>
                     }
                     <nav id="navMenu" className={visible ? "d-block" : "d-none"}>
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <Link class="nav-link active" href="/">Home</Link>
+                        <ul className="nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="/isotube">IsoTube</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/isotube">IsoTube</Link>
                             </li>
-                            <li class="nav-item dropdown">
+                            <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="devicesDropdownLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Sampling Devices
                                 </a>
@@ -81,35 +81,6 @@ export default function Header() {
                         </ul>
                     </nav>
                 </div>
-
-                {/* <nav className="navbar navbar-expand-lg p-0">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <Link className="nav-link" href="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                         <Link className="nav-link" href="/isotube">IsoTube</Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="devicesDropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Sampling Devices
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="devicesDropdownMenuLink">
-                                <ul>
-                                    <li className="dropdown-item"><Link href="/isosampler-pro">IsoSampler Pro</Link></li>
-                                    <li className="dropdown-item"><Link href="#">IsoSampler MAX</Link></li>
-                                    <li className="dropdown-item"><Link href="#">IsoSampler Go</Link></li>
-                                </ul>
-                            </div>
-                        </li>
-                        </ul>
-                    </div>
-                </nav> */}
-
             </div>
         </header>
     )
